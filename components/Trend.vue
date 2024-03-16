102448b19f121e1bfd43fe0bf6a69e45483adbfb
Benoît <benoit.clement@si-4you.com>
2024-03-16 23:47:05 +0100

    feat: useCurrency composable to get properly formatted currency. come computed depending on trending up/down

--- components/Trend.vue
+++ components/Trend.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
     <div class="font-bold" :class="[color]">{{ title }}</div>
+
+    <div class="text-2xl font-extrabold text-black dark:text-white mb-2">
+      <USkeleton class="h-8 w-full" v-if="loading" />
+      <div v-else>{{ currency }}</div>
+    </div>
+
+    <div>
+      <USkeleton class="h-6 w-full" v-if="loading" />
+      <div v-else class="flex space-x-1 items-center text-sm">
+        <UIcon
+          :name="icon"
+          class="w-6 h-6"
+          :class="{ green: trendingUp, red: !trendingUp }"
+        />
+        <div class="text-gray-500 dark:text-gray-400">
+          {{ trendingRate }}% vs last period
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,14 +31,29 @@ const props = defineProps({
   color: String,
   loading: Boolean,
 });
+
+const trendingUp = computed(() => props.amount >= props.lastAmount);
+
+const icon = computed(() => {
+  return trendingUp.value
+    ? "i-heroicons-arrow-trending-up"
+    : "i-heroicons-arrow-trending-down";
+});
+
+const { currency } = useCurrency(props.amount);
+
+const trendingRate = computed(() => {
+  const diff = props.amount - props.lastAmount;
+  return Math.round((Math.abs(diff) * 100) / props.lastAmount);
+});
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .green {
   @apply text-green-600 dark:text-green-400;
 }
 
 .red {
-  @apply;
+  @apply text-red-600 dark:text-red-400;
 }
 </style>
