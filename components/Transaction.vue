fa6a4038b545aca73ebe95d3371926891c777013
Benoît <benoit.clement@si-4you.com>
2024-03-17 14:42:23 +0100

    feat: dropdown for Transaction compo

--- components/Transaction.vue
+++ components/Transaction.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="grid grid-cols-2 py-4 border-b">
+  <div
+    class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-900 dark:text-gray-100"
+  >
     <div class="flex items-center justify-between">
       <div class="flex items-center gap-x-1">
         <UIcon name="i-heroicons-arrow-up-right" class="text-green-600" />
@@ -9,11 +11,33 @@
     </div>
     <div class="flex items-center justify-end gap-x-2">
       <div>{{ currency }}</div>
-      <div><button>Options</button></div>
+      <div>
+        <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+          <UButton
+            color="white"
+            variant="ghost"
+            trailing-icon="i-heroicons-ellipsis-horizontal"
+          />
+        </UDropdown>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 const { currency } = useCurrency(3000);
+const items = [
+  [
+    {
+      label: "Edit",
+      icon: "i-heroicons-pencil-square-20-solid",
+      click: () => console.log("Edit"),
+    },
+    {
+      label: "Delete",
+      icon: "i-heroicons-trash-20-solid",
+      click: () => console.log("Delete"),
+    },
+  ],
+];
 </script>
