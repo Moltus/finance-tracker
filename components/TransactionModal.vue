749bf18c751f806ec903f919021f793268a3c86b
Benoît <benoit.clement@si-4you.com>
2024-03-19 00:20:59 +0100

    refacto: now using Vue 3.4 defineModel() syntax. Parent pass v-model to child which pass it to Nuxt UI component. neat !

--- components/TransactionModal.vue
+++ components/TransactionModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <UModal v-model="isOpen">
+  <UModal v-model="model">
     <UCard>
       <template #header> Add transaction </template>
       <p>Hello</p>
@@ -8,13 +8,5 @@
 </template>
 
 <script setup>
-const props = defineProps({
-  modelValue: Boolean,
-});
-const emit = defineEmits(["update:modelValue"]);
-
-const isOpen = computed({
-  get: () => props.modelValue,
-  set: (value) => emit("update:modelValue", value),
-});
+const model = defineModel();
 </script>
