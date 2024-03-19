b4ee3de00bd66fc56a8df16134631c4cd256f61f
Benoît <benoit.clement@si-4you.com>
2024-03-19 01:30:01 +0100

    feat: TransactionModal form all necessary inputs

--- components/TransactionModal.vue
+++ components/TransactionModal.vue
@@ -2,11 +2,50 @@
   <UModal v-model="model">
     <UCard>
       <template #header> Add transaction </template>
-      <p>Hello</p>
+
+      <div class="flex flex-col gap-y-4">
+        <UFormGroup label="Transaction type" :required="true" name="type">
+          <USelect placeholder="Pick one..." :options="types" v-model="type" />
+        </UFormGroup>
+
+        <UFormGroup label="Amount" :required="true" name="amount">
+          <UInput type="number" placeholder="Enter amount..." />
+        </UFormGroup>
+
+        <UFormGroup label="Transaction date" :required="true" name="created_at">
+          <UInput type="date" icon="i-heroicons-calendar-days-20-solid" />
+        </UFormGroup>
+
+        <UFormGroup label="Description" hint="Optional" name="description">
+          <UInput placeholder="Enter description..." />
+        </UFormGroup>
+
+        <UFormGroup label="Category" name="category">
+          <USelect
+            placeholder="Pick one..."
+            :options="categories"
+            v-model="category"
+          />
+        </UFormGroup>
+      </div>
+
+      <div class="mt-4">
+        <UButton
+          type="submit"
+          color="blue"
+          variant="solid"
+          label="Save"
+          @click="model = false"
+        />
+      </div>
     </UCard>
   </UModal>
 </template>
 
 <script setup>
+import { categories, types } from "~/constants";
 const model = defineModel();
+
+const category = ref(null);
+const type = ref(null);
 </script>
--- constants.js
+++ constants.js
@@ -1,1 +1,9 @@
-export const transactionViewOptions = ['Yearly', 'Monthly', 'Daily']
+export const transactionViewOptions = ["Yearly", "Monthly", "Daily"];
+export const categories = [
+  "Food",
+  "Utilities",
+  "Car",
+  "Entertainment",
+  "Miscellaneous",
+];
+export const types = ["Income", "Expense", "Saving", "Investment"];
