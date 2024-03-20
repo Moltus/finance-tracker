45b86b81b16906a08abfbb97424bcb5d2667929b
Benoît <benoit.clement@si-4you.com>
2024-03-20 23:36:01 +0100

    feat: add color mode selector to header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,10 @@
 <template>
   <header class="flex justify-between items-center mt-10">
     <NuxtLink to="/" class="text-xl font-bold"> Finance Tracker </NuxtLink>
-    <div>
+    <div class="flex gap-2">
+      <ClientOnly>
+        <ColorModeSelector />
+      </ClientOnly>
       <UAvatar
         src="https://avatars.githubusercontent.com/u/739984?v=4"
         alt="Avatar"
