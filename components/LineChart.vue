e0dd074fb9c3beaf11ae30cb3be821c47fa62a15
Benoît <benoit.clement@si-4you.com>
2024-04-03 00:45:26 +0200

    feat: copy pasta line and pie charts

---
+++ components/LineChart.vue
@@ -0,0 +1,161 @@
+<script setup>
+const options = computed(() => ({
+  chart: {
+    type: "line",
+  },
+  title: {
+    text: "Monthly Average Temperature",
+  },
+  subtitle: {
+    text:
+      "Source: " +
+      '<a href="https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature" ' +
+      'target="_blank">Wikipedia.com</a>',
+  },
+  xAxis: {
+    categories: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+  },
+  yAxis: {
+    title: {
+      text: "Temperature (°C)",
+    },
+  },
+  plotOptions: {
+    line: {
+      dataLabels: {
+        enabled: true,
+      },
+      enableMouseTracking: false,
+    },
+  },
+  series: [
+    {
+      name: "Reggane",
+      data: [
+        16.0, 18.2, 23.1, 27.9, 32.2, 36.4, 39.8, 38.4, 35.5, 29.2, 22.0, 17.8,
+      ],
+    },
+    {
+      name: "Tallinn",
+      data: [
+        -2.9, -3.6, -0.6, 4.8, 10.2, 14.5, 17.6, 16.5, 12.0, 6.5, 2.0, -0.9,
+      ],
+    },
+  ],
+  chart: {
+    backgroundColor: {
+      linearGradient: [0, 0, 500, 500],
+      stops: [
+        [0, "rgb(255, 255, 255)"],
+        [1, "rgb(240, 240, 255)"],
+      ],
+    },
+  },
+}));
+
+const pieOptions = computed(() => ({
+  chart: {
+    type: "pie",
+  },
+  title: {
+    text: "Egg Yolk Composition",
+  },
+  tooltip: {
+    valueSuffix: "%",
+  },
+  subtitle: {
+    text: 'Source:<a href="https://www.mdpi.com/2072-6643/11/3/684/htm" target="_default">MDPI</a>',
+  },
+  plotOptions: {
+    series: {
+      allowPointSelect: true,
+      cursor: "pointer",
+      dataLabels: [
+        {
+          enabled: true,
+          distance: 20,
+        },
+        {
+          enabled: true,
+          distance: -40,
+          format: "{point.percentage:.1f}%",
+          style: {
+            fontSize: "1.2em",
+            textOutline: "none",
+            opacity: 0.7,
+          },
+          filter: {
+            operator: ">",
+            property: "percentage",
+            value: 10,
+          },
+        },
+      ],
+    },
+  },
+  series: [
+    {
+      name: "Percentage",
+      colorByPoint: true,
+      data: [
+        {
+          name: "Water",
+          y: 55.02,
+        },
+        {
+          name: "Fat",
+          sliced: true,
+          selected: true,
+          y: 26.71,
+        },
+        {
+          name: "Carbohydrates",
+          y: 1.09,
+        },
+        {
+          name: "Protein",
+          y: 15.5,
+        },
+        {
+          name: "Ash",
+          y: 1.68,
+        },
+      ],
+    },
+  ],
+}));
+</script>
+
+<template>
+  <div>
+    <!-- <highchart :options="options" class="highcharts-light" /> -->
+    <highchart :options="options" />
+  </div>
+</template>
+
+<style scoped>
+@import url("https://code.highcharts.com/dashboards/css/dashboards.css");
+@import url("https://code.highcharts.com/css/highcharts.css");
+@import url("https://code.highcharts.com/dashboards/css/datagrid.css");
+.highcharts-axis-labels {
+  color: #ddd;
+  fill: #ddd !important;
+}
+.highcharts-axis-labels text {
+  color: #ddd;
+  fill: #ddd !important;
+}
+</style>
