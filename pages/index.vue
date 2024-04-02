33e490614bb4dd1b56eed348b74f5c3c2550d915
Benoît <benoit.clement@si-4you.com>
2024-04-02 23:24:53 +0200

    style: remove max width for default layout. Index reworked around display grid (responsive)

--- pages/index.vue
+++ pages/index.vue
@@ -6,77 +6,77 @@
     </div>
   </section>
 
-  <section
-    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10"
-  >
-    <Trend
-      color="green"
-      title="Income"
-      :amount="incomeTotal"
-      :last-amount="prevIncomeTotal"
-      :loading="isPending"
-    />
-    <Trend
-      color="red"
-      title="Expense"
-      :amount="expenseTotal"
-      :last-amount="prevExpenseTotal"
-      :loading="isPending"
-    />
-    <Trend
-      color="green"
-      title="Investments"
-      :amount="4000"
-      :last-amount="5000"
-      :loading="isPending"
-    />
-    <Trend
-      color="red"
-      title="Saving"
-      :amount="4000"
-      :last-amount="3000"
-      :loading="isPending"
-    />
-  </section>
-
-  <section class="flex justify-between mb-10">
-    <div>
-      <h2 class="text-2xl font-extrabold">Transactions</h2>
-      <p class="text-gray-500 dark:text-gray-400">
-        You have
-        {{ incomeCount }}
-        {{ incomeCount > 1 ? "incomes" : "income" }} and
-        {{ expenseCount }}
-        {{ expenseCount > 1 ? "expenses" : "expense" }}
-        this period
-      </p>
-    </div>
-    <div>
-      <TransactionModal v-model="isOpen" @saved="refresh" />
-      <UButton
-        icon="i-heroicons-plus-circle"
-        color="white"
-        variant="solid"
-        label="Add"
-        @click="isOpen = true"
+  <div class="grid grid-cols-1 2xl:grid-cols-2 gap-y-10 2xl:gap-x-16">
+    <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16">
+      <Trend
+        color="green"
+        title="Income"
+        :amount="incomeTotal"
+        :last-amount="prevIncomeTotal"
+        :loading="isPending"
       />
-    </div>
-  </section>
-
-  <section v-if="!isPending">
-    <div v-for="(dayTransactions, date) in byDate" :key="date" class="mb-10">
-      <DailyTransactionSummary :date="date" :transactions="dayTransactions" />
-      <Transaction
-        v-for="transaction in dayTransactions"
-        :key="transaction.id"
-        :transaction="transaction"
-        @deleted="refresh"
+      <Trend
+        color="red"
+        title="Expense"
+        :amount="expenseTotal"
+        :last-amount="prevExpenseTotal"
+        :loading="isPending"
       />
-    </div>
-  </section>
-  <section v-else>
-    <USkeleton class="h-8 w-full mb-2" v-for="i in 4" :key="i" />
-  </section>
+      <Trend
+        color="green"
+        title="Investments"
+        :amount="4000"
+        :last-amount="5000"
+        :loading="isPending"
+      />
+      <Trend
+        color="red"
+        title="Saving"
+        :amount="4000"
+        :last-amount="3000"
+        :loading="isPending"
+      />
+    </section>
+
+    <section class="flex justify-between">
+      <div>
+        <h2 class="text-2xl font-extrabold">Transactions</h2>
+        <p class="text-gray-500 dark:text-gray-400">
+          You have
+          {{ incomeCount }}
+          {{ incomeCount > 1 ? "incomes" : "income" }} and
+          {{ expenseCount }}
+          {{ expenseCount > 1 ? "expenses" : "expense" }}
+          this period
+        </p>
+      </div>
+      <div>
+        <TransactionModal v-model="isOpen" @saved="refresh" />
+        <UButton
+          icon="i-heroicons-plus-circle"
+          color="white"
+          variant="solid"
+          label="Add"
+          @click="isOpen = true"
+        />
+      </div>
+    </section>
+
+    <section v-if="!isPending" class="col-start-1 2xl:col-start-2">
+      <div v-for="(dayTransactions, date) in byDate" :key="date" class="mb-10">
+        <DailyTransactionSummary :date="date" :transactions="dayTransactions" />
+        <Transaction
+          v-for="transaction in dayTransactions"
+          :key="transaction.id"
+          :transaction="transaction"
+          @deleted="refresh"
+        />
+      </div>
+    </section>
+    <section v-else>
+      <USkeleton class="h-8 w-full mb-2" v-for="i in 4" :key="i" />
+    </section>
+  </div>
 </template>
 
 <script setup>
@@ -88,6 +88,8 @@ const selectedView = ref(transactionViewOptions[1]);
 const isOpen = ref(false);
 
 const { current, previous } = useSelectedTimePeriod(selectedView);
+console.log(current.value);
+console.log(previous.value);
 
 const {
   isPending,
