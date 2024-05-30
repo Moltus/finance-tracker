10d5bf17513344ea49fac513fde472f87c50b3bb
Benoît <benoit.clement@si-4you.com>
2024-05-31 00:51:53 +0200

    refacto: script tag first

--- components/TransactionModal.vue
+++ components/TransactionModal.vue
@@ -1,36 +1,3 @@
-<template>
-  <UModal v-model="isOpen">
-    <UCard>
-      <template #header> {{ isEditing ? 'Edit' : 'Add' }} transaction </template>
-
-      <UForm :state="state" :schema="schema" class="flex flex-col gap-y-4" @submit="onSubmit">
-        <UFormGroup label="Transaction type" :required="true" name="type">
-          <USelect :disabled="isEditing" placeholder="Pick one..." :options="types" v-model="state.type" />
-        </UFormGroup>
-
-        <UFormGroup label="Amount" :required="true" name="amount">
-          <UInput type="number" placeholder="Enter amount..." v-model.number="state.amount" />
-        </UFormGroup>
-
-        <UFormGroup label="Transaction date" :required="true" name="created_at">
-          <UInput type="date" icon="i-heroicons-calendar-days-20-solid" v-model="state.created_at" />
-        </UFormGroup>
-
-        <UFormGroup label="Description" hint="Optional" name="description">
-          <UInput placeholder="Enter description..." v-model="state.description" />
-        </UFormGroup>
-
-        <UFormGroup label="Category" name="category" v-if="state.type === 'Expense'">
-          <USelect placeholder="Pick one..." :options="categories" v-model="state.category" />
-        </UFormGroup>
-        <div>
-          <UButton type="submit" color="green" variant="solid" :loading="isLoading">Save</UButton>
-        </div>
-      </UForm>
-    </UCard>
-  </UModal>
-</template>
-
 <script setup>
 import { categories, types } from "~/constants";
 import { z } from "zod";
@@ -138,3 +105,36 @@ watch(isOpen, () => {
   }
 });
 </script>
+
+<template>
+  <UModal v-model="isOpen">
+    <UCard>
+      <template #header> {{ isEditing ? 'Edit' : 'Add' }} transaction </template>
+
+      <UForm :state="state" :schema="schema" class="flex flex-col gap-y-4" @submit="onSubmit">
+        <UFormGroup label="Transaction type" :required="true" name="type">
+          <USelect :disabled="isEditing" placeholder="Pick one..." :options="types" v-model="state.type" />
+        </UFormGroup>
+
+        <UFormGroup label="Amount" :required="true" name="amount">
+          <UInput type="number" placeholder="Enter amount..." v-model.number="state.amount" />
+        </UFormGroup>
+
+        <UFormGroup label="Transaction date" :required="true" name="created_at">
+          <UInput type="date" icon="i-heroicons-calendar-days-20-solid" v-model="state.created_at" />
+        </UFormGroup>
+
+        <UFormGroup label="Description" hint="Optional" name="description">
+          <UInput placeholder="Enter description..." v-model="state.description" />
+        </UFormGroup>
+
+        <UFormGroup label="Category" name="category" v-if="state.type === 'Expense'">
+          <USelect placeholder="Pick one..." :options="categories" v-model="state.category" />
+        </UFormGroup>
+        <div>
+          <UButton type="submit" color="green" variant="solid" :loading="isLoading">Save</UButton>
+        </div>
+      </UForm>
+    </UCard>
+  </UModal>
+</template>
